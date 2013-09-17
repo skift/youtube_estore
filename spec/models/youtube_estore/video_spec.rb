b82bab39a4fe9cd35800c94bc29bf3252f88555e
Dan Nguyen <dansonguyen@gmail.com>
2013-09-17 17:09:02 -0400

    sample json

--- spec/models/youtube_estore/video_spec.rb
+++ spec/models/youtube_estore/video_spec.rb
@@ -5,3 +5,16 @@ module YoutubeEstore
     pending "add some examples to (or delete) #{__FILE__}"
   end
 end
+
+
+
+describe 'video' do 
+
+  context 'hello world' do 
+    it 'should be true' do
+      expect(true).to be_true
+    end
+  end
+
+end
+
--- spec/support/mock_user.rb
+++ spec/support/mock_user.rb
@@ -1,11 +1,11 @@
 ActiveRecord::Schema.define do
     create_table :youtube_estore_users do |t|
-        t.integer  :age 
-        t.string   :company 
-        t.string   :description 
-        t.string   :last_login 
-        t.string   :location 
-        t.datetime   :join_date 
+        t.integer   :age 
+        t.string    :company 
+        t.string    :description 
+        t.string    :last_login 
+        t.string    :location 
+        t.datetime  :join_date 
         t.integer   :subscribers 
         t.integer   :upload_views 
         t.string    :username 
