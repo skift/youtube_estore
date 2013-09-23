5402d4297f5b2ad8df0e0581d37f6d539372c3f9
Dan Nguyen <dansonguyen@gmail.com>
2013-09-23 13:39:46 -0400

    schema fun

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -14,16 +14,14 @@
 ActiveRecord::Schema.define(:version => 20130917185900) do
 
   create_table "youtube_estore_channels", :force => true do |t|
-    t.integer  "age"
-    t.string   "description"
-    t.integer  "subscriber_count"
-    t.integer  "video_count"
-    t.string   "username"
-    t.integer  "view_count"
-    t.string   "default_thumbnail"
-    t.string   "t_id"
-    t.datetime "rails_created_at"
-    t.datetime "rails_updated_at"
+    t.integer "age"
+    t.string  "description"
+    t.integer "subscriber_count"
+    t.integer "video_count"
+    t.string  "username"
+    t.integer "view_count"
+    t.string  "default_thumbnail"
+    t.string  "t_id"
   end
 
   create_table "youtube_estore_videos", :force => true do |t|
